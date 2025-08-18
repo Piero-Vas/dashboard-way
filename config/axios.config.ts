21b6faaf8dce2ccebaba409cc9974fd0be3ea3ba
Piero-Vas <piero.vasquezriveros@gmail.com>
2025-08-18 10:38:37 -0500

    refactor: remove outdated registration and verification pages and forms; update axios base URL

--- config/axios.config.ts
+++ config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = process.env.NEXT_PUBLIC_SITE_URL;
-const baseURL = 'http://34.67.41.175/api/1.0';
+const baseURL = 'http://34.67.41.175/backend/1.0';
 
 export const api = axios.create({
   baseURL,
